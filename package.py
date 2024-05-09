ee292c41ada8eee80430a0f58cb6ff01cf12238a
Nick <nicknoto02@protonmail.com>
2024-05-09 15:17:24 -0400

    Add state attribute

--- package.py
+++ package.py
@@ -1,8 +1,9 @@
 class Package:
-    def __init__(self, ID, address, city, zipcode, deadline, weight, status):
+    def __init__(self, ID, address, city, state, zipcode, deadline, weight, status):
         self.ID = ID
         self.address = address
         self.city = city
+        self.sate = state
         self.zipcode = zipcode
         self.deadline = deadline
         self.weight = weight
@@ -12,6 +13,7 @@ class Package:
         return (f"ID: {self.ID}   "
                 f"Address: {self.address}   "
                 f"City: {self.city}   "
+                f"State: {self.sate}   "
                 f"Zipcode: {self.zipcode}   "
                 f"Deadline: {self.deadline}   "
                 f"Weight: {self.weight}   "
