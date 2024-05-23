e4c90f941c3cee6436fbc5ebb5728ea86e875cad
Nick <nicknoto02@protonmail.com>
2024-05-23 13:34:15 -0400

    Create 3 truck objects and load packages on them.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
+import datetime
 
+import truck
 from hashTable import HashTable
 from truck import Truck
 from package import Package
@@ -43,3 +45,13 @@ package_hashmap = HashTable()
 
             # Loads the data from Package.csv in package_hashmap
 load_package_data("data/Packages.csv", package_hashmap)
+
+
+truck1 = truck.Truck(16, None, [1, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40], 18, 0.0,
+                     "4001 South 700 East", datetime.timedelta(hours=8))
+
+truck2 = truck.Truck(16, None, [2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 17, 18, 21, 22, 36, 38], 18, 0.0,
+                     "4001 South 700 East", datetime.timedelta(hours=10, minutes=20))
+
+truck3 = truck.Truck(16, None, [6, 23, 24, 25, 26, 27, 28, 32, 33, 35, 39], 18, 0.0,
+                     "4001 South 700 East", datetime.timedelta(hours=9, minutes=5))
